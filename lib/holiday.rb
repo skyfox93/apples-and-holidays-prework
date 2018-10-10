c763e1cb5a5af9533073e7d86f74df571157c173
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-10 14:52:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,21 @@ def all_holidays_with_bbq(holiday_hash)
  with_bbq
 end
 
-all_holidays_with_bbq({})
+all_holidays_with_bbq( {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+})
 
 
 
