a773a17f949a7d6ef02d9ddd2f4e0c4bbb2a763b
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-10 15:10:52 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,6 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.each do |holiday,supplies|
       if supplies.include? "BBQ"
        with_bbq << holiday
+       binding.pry
       end
     end
  end
